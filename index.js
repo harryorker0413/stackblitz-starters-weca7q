afa2bf48739539408ca7d3895ea494bb94ff86ea
harryorker0413 <harry.worker.tsai@gmail.com>
2024-10-31 21:48:05 +0800

    test

--- index.js
+++ index.js
@@ -1,28 +1,18 @@
 const Koa = require('koa');
 const logger = require('koa-logger');
 const router = require('@koa/router')();
-const bodyParser = require('koa-bodyparser');
 
 const app = new Koa();
 
 // middlewares
+
 app.use(logger());
-app.use(bodyParser());
 
 const helloWorld = (ctx) => {
-  ctx.body = 'Hello World!!!0000';
-};
-
-const add = (ctx) => {
-  const { a, b } = ctx.query;
-  const sum = Number(a) + Number(b);
-  ctx.body = {
-    result: sum,
-  };
+  ctx.body = 'Hello World!!!SSSS';
 };
 
 router.get('/', helloWorld);
-router.get('/add', add);
 
 app.use(router.routes());
 
